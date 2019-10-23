efccde2c033fc3fe6034f8affc8fafb8bde020fc
mariaandreescu <48623441+mariaandreescu@users.noreply.github.com>
2019-10-23 20:22:39 +0300

    Created hello world

--- AdvertisingCampaignManagement/app/src/main/java/com/adobe/devcamp/HelloWorld.java
+++ AdvertisingCampaignManagement/app/src/main/java/com/adobe/devcamp/HelloWorld.java
@@ -1,9 +1,18 @@
 package com.adobe.devcamp;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+    public static void main(String[] args) throws Exception{
 
+        // Loading driver
+        Class.forName("com.mysql.jdbc.Driver");
+        // Getting connection
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/javadevcamp",
+                "root",
+                "devcamp");
+    }
 }
