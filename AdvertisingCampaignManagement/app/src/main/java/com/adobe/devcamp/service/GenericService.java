5f8da225553215e3621a8cb086ee677e530e53d0
mariaandreescu <48623441+mariaandreescu@users.noreply.github.com>
2019-10-30 20:14:00 +0200

    Day 3

--- AdvertisingCampaignManagement/app/src/main/java/com/adobe/devcamp/service/GenericService.java
+++ AdvertisingCampaignManagement/app/src/main/java/com/adobe/devcamp/service/GenericService.java
@@ -2,7 +2,7 @@ package com.adobe.devcamp.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.adobe.devcamp.dao.UserDao;
+import com.adobe.devcamp.dao.GenericDao;
 import com.adobe.devcamp.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,17 +11,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class UserService {
-    private final UserDao userDao;
+    private final GenericDao genericDao;
     private final ObjectMapper objectMapper;
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
-    public UserService(UserDao userDao, ObjectMapper objectMapper) {
-        this.userDao = userDao;
+    public UserService(GenericDao genericDao, ObjectMapper objectMapper) {
+        this.genericDao = genericDao;
         this.objectMapper = objectMapper;
     }
 
     public Map<Integer, User> getUsers() {
-        final Map<Integer, String> usersMap = userDao.selectUsers();
+        final Map<Integer, String> usersMap = genericDao.selectAll();
         final Map<Integer, User> users = new HashMap<>();
         for (Map.Entry<Integer, String> entry: usersMap.entrySet()) {
             final User user;
